f160b17283005b0f7120fb6f9f82309cce2d399b
Riyad139 <riyad.ahmed1886@gmail.com>
2024-11-23 05:24:08 +0600

    added

--- components/NavBar/MainNav.tsx
+++ components/NavBar/MainNav.tsx
@@ -29,17 +29,17 @@ export default function MainNav({
     {
       href: `/${params.storeId}/sizes`,
       label: "Sizes",
-      active: pathname === `/${params.storeId}/Sizes`,
+      active: pathname === `/${params.storeId}/sizes`,
     },
     {
       href: `/${params.storeId}/colors`,
       label: "Colors",
-      active: pathname === `/${params.storeId}/Colors`,
+      active: pathname === `/${params.storeId}/colors`,
     },
     {
       href: `/${params.storeId}/products`,
       label: "Products",
-      active: pathname === `/${params.storeId}/Products`,
+      active: pathname === `/${params.storeId}/products`,
     },
     {
       href: `/${params.storeId}/settings`,
